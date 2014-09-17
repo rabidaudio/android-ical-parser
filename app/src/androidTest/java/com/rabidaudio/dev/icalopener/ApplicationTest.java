c8b5d97ddeaece1bf379f3a5494fb6f875da758f
Charles Julian Knight <charles@rabidaudio.com>
2014-09-17 00:58:30 -0400

    split test into multiple tests

--- app/src/androidTest/java/com/rabidaudio/dev/icalopener/ApplicationTest.java
+++ app/src/androidTest/java/com/rabidaudio/dev/icalopener/ApplicationTest.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.content.Intent;
 import android.provider.CalendarContract;
 import android.test.ActivityTestCase;
+import android.test.ActivityUnitTestCase;
 import android.test.ApplicationTestCase;
 
 import junit.framework.Assert;
@@ -18,45 +19,74 @@ import java.util.Date;
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ActivityTestCase {
+public class ApplicationTest extends ActivityUnitTestCase<MyActivity> {
+    public ApplicationTest(){
+        super(MyActivity.class);
+    }
 
-    public void testProperFileParsing() throws Exception {
-        InputStream is = getInstrumentation().getTargetContext().getResources().openRawResource(R.raw.testfile2);
+    InputStream is;
+    ICALParser parser;
+    Intent output;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        is = getInstrumentation().getTargetContext().getResources().openRawResource(R.raw.testfile2);
 
         Assert.assertNotNull("Test file is available", is);
 
-        ICALParser parser = new ICALParser(is);
+        parser = new ICALParser(is);
 
         Assert.assertNotNull("Parser is created", parser);
 
-        Intent output = parser.buildIntent();
+        output = parser.buildIntent();
 
         Assert.assertNotNull("Intent is created", output);
-        Assert.assertEquals("Proper Action", "content://com.android.calendar/events", output.getAction());
+    }
 
-        //values
+
+
+
+    public void testBasicIntentProperties() throws Exception {
+
+        Assert.assertEquals("Proper Action", Intent.ACTION_INSERT, output.getAction());
+        Assert.assertEquals("Proper URI", "content://com.android.calendar/events", output.getDataString());
+    }
+
+    public void testTitleDescription() throws Exception {
         String title = output.getStringExtra(CalendarContract.Events.TITLE);
+        String location = output.getStringExtra(CalendarContract.Events.EVENT_LOCATION);
+        String description = output.getStringExtra(CalendarContract.Events.DESCRIPTION);
+
+        Assert.assertEquals("Proper title", "Bass lesson", title);
+        Assert.assertEquals("Proper location", "Atlanta, GA, USA", location);
+        Assert.assertEquals("Proper description", "something", description);
+
+    }
+
+    public void testProperTime() throws Exception {
         long startTime = output.getLongExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, -1);
         long endTime = output.getLongExtra(CalendarContract.EXTRA_EVENT_END_TIME, -1);
         boolean allDay = output.getBooleanExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, true);
-        String location = output.getStringExtra(CalendarContract.Events.EVENT_LOCATION);
-        String description = output.getStringExtra(CalendarContract.Events.DESCRIPTION);
+
+        Assert.assertEquals("Proper start time", new Date(2014, 7, 23, 18, 0).getTime(), startTime);
+        Assert.assertEquals("Proper end time", new Date(2014, 7, 23, 19, 0).getTime(), endTime);
+        //Assert.assertFalse("Proper full-day event status", allDay);
+    }
+
+    public void testProperExtendedFields() throws Exception {
         String email = output.getStringExtra(Intent.EXTRA_EMAIL);
         String rrule = output.getStringExtra(CalendarContract.Events.RRULE);
         //Events.ACCESS_LEVEL
         //Events.AVAILABILITY
 
-        Assert.assertEquals("Proper title", "Bass lesson", title);
-
-        Assert.assertEquals("Proper start time", new Date(2014, 7, 23, 18, 0).getTime(), startTime);
-        Assert.assertEquals("Proper end time", new Date(2014, 7, 23, 19, 0).getTime(), endTime);
-        //Assert.assertFalse("Proper full-day event status", allDay);
-
-        Assert.assertEquals("Proper location", "Atlanta, GA, USA", location);
-        Assert.assertEquals("Proper description", "something", description);
-
         Assert.assertEquals("Proper repetition rules", "FREQ=WEEKLY;WKST=SU;INTERVAL=2;BYDAY=WE", rrule);
+    }
 
-
+    @Override
+    protected void tearDown() throws Exception{
+        is.close();
+        super.tearDown();
     }
 }
