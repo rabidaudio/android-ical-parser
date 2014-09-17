b5b750183c4be8bde47713458b493bdb5942f014
Charles Julian Knight <charles@rabidaudio.com>
2014-09-17 00:07:38 -0400

    working test suite setup

--- app/src/androidTest/java/com/rabidaudio/dev/icalopener/ApplicationTest.java
+++ app/src/androidTest/java/com/rabidaudio/dev/icalopener/ApplicationTest.java
@@ -1,15 +1,26 @@
 package com.rabidaudio.dev.icalopener;
 
+import android.app.Activity;
 import android.app.Application;
+import android.test.ActivityTestCase;
 import android.test.ApplicationTestCase;
+
+import junit.framework.Assert;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+public class ApplicationTest extends ActivityTestCase {
+
+    public void testProperFileParsing() throws Exception {
+        InputStream is = getInstrumentation().getTargetContext().getResources().openRawResource(R.raw.testfile);
+
+        BufferedReader r = new BufferedReader(new InputStreamReader(is));
+        Assert.assertEquals(r.readLine(), "BEGIN:VCALENDAR");
     }
-
-
 }
